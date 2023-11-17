ac51947956e0ca8115ceabfa64a20e22ddb87d21
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2023-11-18 01:34:34 +0700

    handle listprinter, sreach

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import './Login.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Input({type, id, name, label, placeholder, autofocus}) {
+function Input({ type, id, name, label, placeholder, autofocus }) {
   return (
     <div>
       <div className="d-flex align-items-start mx-4">{label}</div>
       <input
         autoFocus={autofocus}
-        type={type} 
-        id={id} 
-        name={name} 
+        type={type}
+        id={id}
+        name={name}
         placeholder={placeholder}
         className="input_box rounded px-4 py-3 w-full mt-1 bg-white text-gray-900 border border-gray-200 focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-100"
       />
@@ -23,21 +23,21 @@ function InputForm() {
   return (
     <div className="d-flex flex-column justify-content-start w-100 rounded-circle">
       <Input
-        type="email" 
-        id="email" 
-        name="email" 
-        label="Email Address" 
-        placeholder="me@example.com" 
+        type="email"
+        id="email"
+        name="email"
+        label="Email Address"
+        placeholder="me@example.com"
         autofocus={true}
       />
-      <Input 
-        type="password" 
-        id="password" 
-        name="password" 
-        label="Password" 
-        placeholder="••••••••••" 
+      <Input
+        type="password"
+        id="password"
+        name="password"
+        label="Password"
+        placeholder="••••••••••"
       />
-      <Link to='/buy'>
+      <Link to='/'>
         <button className="button_box w-50 h-40 text-white my-2">
           Đăng Nhập
         </button>
@@ -50,25 +50,25 @@ const Login = () => {
   const logoBK = require('./../../assets/Image/logoBK.png')
   const google = require('./../../assets/Image/google-icon.png')
   return (
-    <div className = "login_bg">
-      <div className = "login_form">
-        <div className = "box">
-          <div className = "logo_box">
-            <img src={logoBK} alt='logo_BK' className='logo_BK' />  
+    <div className="login_bg">
+      <div className="login_form">
+        <div className="box">
+          <div className="logo_box">
+            <img src={logoBK} alt='logo_BK' className='logo_BK' />
           </div>
-          <div className = "content_box">
-            <div className = "register">
+          <div className="content_box">
+            <div className="register">
               Bạn chưa có tài khoản?
               Đăng kí ngay!
             </div>
-            <div className = "login_using_google">
-              <img src={google} alt='google' className='google' />  
+            <div className="login_using_google">
+              <img src={google} alt='google' className='google' />
               Đăng nhập bằng Google
             </div>
 
-            <div className = "input_box_layout">
+            <div className="input_box_layout">
               <InputForm></InputForm>
-                <div className = "label_content">Quên mật khẩu</div>            
+              <div className="label_content">Quên mật khẩu</div>
             </div>
           </div>
         </div>
